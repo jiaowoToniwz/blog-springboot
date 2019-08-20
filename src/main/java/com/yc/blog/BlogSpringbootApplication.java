8f2f09759e279574c5514ce73046b95603e86c78
jiaowoToniwz <54107365+jiaowoToniwz@users.noreply.github.com>
2019-08-20 19:24:33 +0800

    0820 idea环境

--- src/main/java/com/yc/blog/BlogSpringbootApplication.java
+++ src/main/java/com/yc/blog/BlogSpringbootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 @Controller
-@MapperScan("com.yc")
+@MapperScan("com.yc.blog.dao")
 public class BlogSpringbootApplication {
 
 	public static void main(String[] args) {
